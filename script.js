882e672d68e5612e118c25d18e3cbf651163590f
AdityPratap <93519627+AdityPratap@users.noreply.github.com>
2024-08-14 21:02:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Basic JavaScript for navigation (scrolling effects)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
